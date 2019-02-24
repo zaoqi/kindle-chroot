07f9cbad8c10fa996e9b4f1ea9530fbb49d3d04f
zaoqi <zaomir@outlook.com>
2019-02-24 16:30:32 +0800

    .

--- run.alpine__3.9.0.kterm.sh
+++ run.alpine__3.9.0.kterm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-/mnt/us/extensions/kterm/bin/kterm.sh -e "$0"'/run-distribution.sh alpine.3.9.0.armhf.sh'
+/mnt/us/extensions/kterm/bin/kterm.sh -e "$PWD"'/run-distribution.sh alpine.3.9.0.armhf.sh'
